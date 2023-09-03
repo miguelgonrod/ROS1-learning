8fc4ce1a3a0e4b4f9a5fa8c0336ba770bf815530
miguelgonrod <miguelgonrod2004@gmail.com>
2023-09-03 16:08:30 -0500

    Added the client and server for services

--- src/hrwros/hrwros_week1/scripts/meters_to_feet_server.py
+++ src/hrwros/hrwros_week1/scripts/meters_to_feet_server.py
@@ -22,12 +22,13 @@ def process_service_request(req):
         res.distance_feet = _CONVERSION_FACTOR_METERS_TO_FEET * req.distance_meters
         res.success = True
         
+        
     # return the response message
     return res
 
 def meters_to_feet_server():
     # ROS node for the service server.
-    rospy.init_node('meters_to_feet_server', anjonymous=False)
+    rospy.init_node('meters_to_feet_server', anonymous=False)
     
     # create a ROS service type
     service = rospy.Service('meters_to_feet', ConvertMeterToFeet, process_service_request)
